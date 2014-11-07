0fc6e55945f0682ebf1eacc1606de220b147a0ae
Jordan Hatch <jordan@jordanh.net>
2014-11-07 14:16:56 +0000

    Change LatestChanges to support a start parameter
    This changes the LatestChanges model so that the 'find' method can be 
    called with a 'start' argument. 
    
    When the 'start' argument is provided, we pass this into the query made 
    to Rummager. When it isn't provided, we default to zero. (this maintains
    the existing behaviour)

--- app/models/latest_changes.rb
+++ app/models/latest_changes.rb
@@ -1,9 +1,9 @@
 require "active_support/core_ext/hash/indifferent_access"
 
 class LatestChanges
-  def self.find(slug)
+  def self.find(slug, search_query_opts={})
     search_client = CollectionsAPI.services(:rummager)
-    search_query = build_search_query_for(slug)
+    search_query = build_search_query_for(slug, search_query_opts)
 
     if (contents = search_client.unified_search(search_query)) && contents["results"]
       return self.new(contents)
@@ -12,9 +12,10 @@ class LatestChanges
     end
   end
 
-  def self.build_search_query_for(slug)
+  def self.build_search_query_for(slug, options)
     {
      count: "50",
+     start: options.fetch(:start, 0).to_s,
      filter_specialist_sectors: [slug],
      order: "-public_timestamp",
      fields: %w(title link latest_change_note public_timestamp)
@@ -27,5 +28,13 @@ class LatestChanges
 
   def results
     @contents["results"].map(&:with_indifferent_access)
+  end
+
+  def start
+    @contents['start'].to_i
+  end
+
+  def total
+    @contents['total'].to_i
   end
 end
